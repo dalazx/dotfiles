3bcb6d291ae7fa60e3e727fafff51120e0f16eea
Aleksandr Danshyn <dalazx@gmail.com>
2014-03-24 19:58:45 +0200

    Added pip to install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ sudo apt-get install nautilus-dropbox
 sudo apt-get install git-core
 sudo apt-get install subversion
 sudo apt-get install build-essential cmake python-dev
+sudo apt-get install pkg-config
 
 ln -s ~/Dropbox/.ssh ~/.ssh
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
@@ -24,11 +25,18 @@ ln -s ~/.dotfiles/vimrc ~/.vimrc
 
 git clone https://github.com/gmarik/vundle.git vim/bundle/vundle
 
+sudo apt-get install python-setuptools
+sudo easy_install pip
+sudo pip install -U distribute pip
+sudo pip install virtualenv
+
 pip install pyprof2calltree
 
 sudo apt-get install vim-gtk
 
 sudo pip install pylama
 
+sudo apt-get install virtualbox
+# vagrant
 
 mkdir ~/dev
